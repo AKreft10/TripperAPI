abbafbc633bc6505cc732d3d79f77828860abce3
AKreft10 <arkadiuszkreft8@gmail.com>
2022-05-12 16:57:25 +0200

    Create class to handle results from Bing maps API

---
+++ TripperAPI/Services/PlaceDistanceService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TripperAPI.Services
+{
+    public class PlaceDistanceService : IPlaceDistanceService
+    {
+        public IDictionary<string, double> GetTheNearestPlaces()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
